5818cf0e8552495e568008da3822e302261e0b21
Ozh <ozh@ozh.org>
2022-04-13 22:13:34 +0200

    Deal with when not in a YOURLS install

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,13 +13,26 @@
 define('YOURLS_ABSPATH', dirname(__DIR__).'/YOURLS');
 
 /**
- * Other paths belong to current YOURLS root - otherwise we would not be able to load the plugin
- * we're testing since the YOURLS test suite locates plugins in <YOURLS ROOT>/tests/data/
+ * Other paths set to the current plugin directory
  */
+
+// Check if the plugin is within a YOURLS install or not
 define('YOURLS_PARENT_USER', dirname(dirname(dirname(dirname(__DIR__)))));
-define('YOURLS_PLUGINDIR',   YOURLS_PARENT_USER.'/plugins/');
-define('YOURLS_PAGEDIR',     YOURLS_PARENT_USER.'/pages/');
-define('YOURLS_LANG_DIR',    YOURLS_PARENT_USER.'/languages/');
+if(
+       file_exists(YOURLS_PARENT_USER . '/plugins')
+    && file_exists(YOURLS_PARENT_USER . '/languages')
+    && file_exists(YOURLS_PARENT_USER . '/pages')
+) {
+    // It seems that the current plugin is whithin an actual YOURLS install : use the YOURLS directories
+    define('YOURLS_PLUGINDIR',   YOURLS_PARENT_USER.'/plugins/');
+    define('YOURLS_PAGEDIR',     YOURLS_PARENT_USER.'/pages/');
+    define('YOURLS_LANG_DIR',    YOURLS_PARENT_USER.'/languages/');
+} else {
+    // Otherwise use whatever parent parent parent, in order to comply to yourls_activate_plugin() expectations
+    define('YOURLS_PLUGINDIR',   dirname(dirname(dirname(__DIR__))));
+}
+
+echo "YOURLS_PLUGINDIR: ".YOURLS_PLUGINDIR."\n";
 
 // YOURLS Unit tests use a sample language file to test translations - don't use it here
 define('YOURLS_LANG', '');
@@ -30,7 +43,7 @@ error_reporting(E_ALL & ~E_WARNING);
 
 // Load YOURLS and its test suite
 if(!file_exists(dirname(__DIR__) . '/YOURLS/tests/bootstrap.php')) {
-    fwrite(STDERR, "YOURLS test suite not found. Please run script `install-test-suite.sh`\n");
+    echo "YOURLS test suite not found. Please run script `install-test-suite.sh`\n";
     exit(1);
 }
 require_once dirname(__DIR__) . '/YOURLS/tests/bootstrap.php';
@@ -46,7 +59,7 @@ $plugin_data = yourls_get_plugin_data($plugin);
 $activate = yourls_activate_plugin($plugin);
 
 if( $activate !== true ) {
-    fwrite(STDERR, "Failed to activate plugin. Error was: $activate\n");
+    echo "Failed to activate plugin. Error was: $activate\n";
     exit(1);
 }
 
